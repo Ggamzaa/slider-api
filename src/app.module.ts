be720ca9aceba8d713a438a3d29333dc384cb076
Ggamzaa <sungsu52198@gmail.com>
2025-08-25 21:53:01 +0900

    TypeOrm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './orm.config';
 
 @Module({
   imports: [
    ConfigModule.forRoot({
       isGlobal: true, // 전역에서 process.env 바로 사용 가능
     }),
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
   ],
   controllers: [AppController],
   providers: [AppService],
